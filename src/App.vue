fa6118a3d75e2eb69eb3c8903312b25073c5ace4
XUHUANSHENHE <xushen18683205265@outlook.com>
2025-12-03 06:57:07 +0800

    用户协议增加 以及 移动端适配

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,31 @@ import TaleMaker from './components/TaleMaker.vue';
 </script>
 
 <template>
+  <div id="app">
     <TaleMaker msg="TaleMaker" />
+  </div>
 </template>
+
+<style>
+/* 隐藏整个页面的滚动条 */
+html,
+body {
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+
+/* 隐藏所有滚动条 */
+::-webkit-scrollbar {
+  display: none;
+}
+
+* {
+  -ms-overflow-style: none;
+  /* IE and Edge */
+  scrollbar-width: none;
+  /* Firefox */
+}
+</style>
